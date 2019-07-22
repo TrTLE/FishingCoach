0eb29187a5c8aa4b1a0370432107ddbefcee8ea9
chpaqouqou <chpaqou@gmail.com>
2019-07-22 09:54:13 +0200

    COMMIT 19-22-07
    First screen improve auto sizing pictures

--- app/src/main/java/io/fishingcoach/MainActivity.kt
+++ app/src/main/java/io/fishingcoach/MainActivity.kt
@@ -2,11 +2,29 @@ package io.fishingcoach
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get device dimensions
+        val displayMetrics = DisplayMetrics()
+
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+
+
+        var width = displayMetrics.widthPixels
+        var height = displayMetrics.heightPixels
+
+        var heightPicture = ( height / 4 ) - 50
+
+        //pond.height = heightPicture
+
+
     }
 }
